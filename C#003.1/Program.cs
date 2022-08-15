936932598f4c8a9e18259421e676341f8727fe42
Алексей <110294020+Alexeii-Toropov@users.noreply.github.com>
2022-08-15 17:24:55 +0300

    Add files via upload

---
+++ C#003.1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+int pow = 0;
+while (number > 0)
+{
+    pow = Convert.ToInt32(Math.Pow(number,3));
+    Console.Write(pow + " ");
+    pow = 0;
+    number--;
+}
