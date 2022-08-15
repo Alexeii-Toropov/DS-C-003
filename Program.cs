16146c7b86001436becdfeea989909279ab10677
Alexeii Toropov <leha.toropov.04@mail.ru>
2022-08-15 17:07:42 +0300

    finish

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите пятизначное число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+string stnumber = Convert.ToString(number);
+if (stnumber[0] == stnumber[4])
+{
+    if (stnumber[1] == stnumber[3])
+    {
+        Console.Write("да");
+    }
+    else
+    {
+        Console.Write("нет");
+    }
+}
+else
+{
+    Console.Write("нет");
+}
